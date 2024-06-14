9e80b879497a94ae1605c7d6154169a2b7bb551e
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2024-06-14 00:22:28 -0700

    Progress bar PoC

--- includes/Admin/Settings.php
+++ includes/Admin/Settings.php
@@ -9,7 +9,7 @@ namespace WC\SmoothGenerator\Admin;
 
 use Automattic\WooCommerce\Internal\BatchProcessing\BatchProcessingController;
 use Automattic\WooCommerce\StoreApi\Routes\V1\Batch;
-use WC\SmoothGenerator\Admin\BatchProcessor;
+use WC\SmoothGenerator\Admin\{ BatchProcessor, AsyncJob };
 
 /**
  *  Initializes and manages the settings screen.
@@ -24,6 +24,7 @@ class Settings {
 	 */
 	public static function init() {
 		add_action( 'admin_menu', array( __CLASS__, 'register_admin_menu' ) );
+		add_filter( 'heartbeat_received', array( __CLASS__, 'receive_heartbeat' ), 10, 3 );
 	}
 
 	/**
@@ -45,29 +46,181 @@ class Settings {
 	 * Render the admin page.
 	 */
 	public static function render_admin_page() {
+		$current_job = self::get_current_job();
+
+		$generate_button_atts = $current_job instanceof AsyncJob ? array( 'disabled' => true ) : array();
+		$cancel_button_atts   = ! $current_job instanceof AsyncJob ? array( 'disabled' => true ) : array();
+
 		?>
 		<h1>WooCommerce Smooth Generator</h1>
+		<p class="description">
+			Generate randomized WooCommerce data for testing.
+		</p>
+
 		<?php echo self::while_you_wait(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+
+		<?php if ( $current_job instanceof AsyncJob ) : ?>
+			<div id="smoothgenerator-progress">
+				<label for="smoothgenerator-progress-bar" style="display: block;">
+					<?php
+					printf(
+						'Generating %s&hellip;',
+						esc_html( $current_job->generator_slug )
+					);
+					?>
+				</label>
+				<progress
+					id="smoothgenerator-progress-bar"
+					max="<?php echo esc_attr( $current_job->amount ); ?>"
+					value="<?php echo $current_job->processed ? esc_attr( $current_job->processed ) : ''; ?>"
+					style="width: 560px;"
+				>
+					<?php
+					printf(
+						'%d out of %d',
+						esc_html( $current_job->processed ),
+						esc_html( $current_job->amount ),
+					);
+					?>
+				</progress>
+			</div>
+		<?php elseif ( filter_input( INPUT_POST, 'cancel_job' ) ) : ?>
+			<div class="notice notice-error inline-notice is-dismissible" style="margin-left: 0;">
+				<p>Canceling current job...</p>
+			</div>
+		<?php endif; ?>
+
 		<form method="post">
 			<?php wp_nonce_field( 'generate', 'smoothgenerator_nonce' ); ?>
-			<h2>Generate Products</h2>
+			<h2>Generate products</h2>
 			<p>
-				<input type="number" name="num_products_to_generate" value="<?php echo esc_attr( self::DEFAULT_NUM_PRODUCTS ); ?>" min="1" />
-				Number of products to generate.
+				<label for="smoothgenerator-products-input" class="screen-reader-text">Number of products to generate</label>
+				<input
+					id="smoothgenerator-products-input"
+					type="number"
+					name="num_products_to_generate"
+					value="<?php echo esc_attr( self::DEFAULT_NUM_PRODUCTS ); ?>"
+					min="1"
+					<?php disabled( $current_job instanceof AsyncJob ); ?>
+				/>
+				<?php
+				submit_button(
+					'Generate',
+					'primary',
+					'generate_products',
+					false,
+					$generate_button_atts
+				);
+				?>
 			</p>
-			<?php submit_button( 'Generate', 'primary', 'generate_products' ); ?>
 
-			<h2>Generate Orders</h2>
+			<h2>Generate orders</h2>
 			<p>
-				<input type="number" name="num_orders_to_generate" value="<?php echo esc_attr( self::DEFAULT_NUM_ORDERS ); ?>" min="1" />
-				Number of orders to generate.
+				<label for="smoothgenerator-orders-input" class="screen-reader-text">Number of orders to generate</label>
+				<input
+					id="smoothgenerator-orders-input"
+					type="number"
+					name="num_orders_to_generate"
+					value="<?php echo esc_attr( self::DEFAULT_NUM_ORDERS ); ?>"
+					min="1"
+					<?php disabled( $current_job instanceof AsyncJob ); ?>
+				/>
+				<?php
+				submit_button(
+					'Generate',
+					'primary',
+					'generate_orders',
+					false,
+					$generate_button_atts
+				);
+				?>
 			</p>
-			<?php submit_button( 'Generate', 'primary', 'generate_orders' ); ?>
 
-			<h2>Cancel all scheduled generations</h2>
-			<?php submit_button( 'Cancel all', 'primary', 'cancel_all_generations' ); ?>
+			<?php
+			submit_button(
+				'Cancel current job',
+				'secondary',
+				'cancel_job',
+				true,
+				$cancel_button_atts
+			);
+			?>
 		</form>
 		<?php
+
+		self::heartbeat_script();
+	}
+
+	/**
+	 * Script to run the progress bar.
+	 *
+	 * @return void
+	 */
+	protected static function heartbeat_script() {
+		?>
+		<script>
+			( function( $ ) {
+				var $progress = $( '#smoothgenerator-progress-bar' );
+
+				$( document ).on( 'ready', function () {
+					wp.heartbeat.connectNow();
+					wp.heartbeat.interval( 'fast' );
+				} );
+
+				$( document ).on( 'heartbeat-send', function ( event, data ) {
+					data.smoothgenerator = 'check_async_job_progress';
+				} );
+
+				$( document ).on( 'heartbeat-tick', function ( event, data ) {
+					$.ajax( {
+						url: data.smoothgenerator_ping_cron,
+						method: 'get',
+						timeout: 5000,
+						dataType: 'html'
+					} );
+
+					if ( 'object' === typeof data.smoothgenerator_async_job_progress ) {
+						var value = parseInt( data.smoothgenerator_async_job_progress.processed );
+						if ( value > 0 ) {
+							$progress.prop( 'value', value );
+						}
+					} else if ( 'complete' === data.smoothgenerator_async_job_progress ) {
+						var max = $progress.prop( 'max' );
+						$progress.prop( 'value', max );
+						$progress.parent().append( '✅' );
+						$( document ).off( 'heartbeat-send' );
+						$( document ).off( 'heartbeat-tick' );
+					}
+				} );
+			} )( jQuery );
+		</script>
+	<?php
+	}
+
+	/**
+	 * Callback to send data for updating the progress bar.
+	 *
+	 * @param array  $response  The data that will be sent back to heartbeat.
+	 * @param array  $data      The incoming data from heartbeat.
+	 * @param string $screen_id The ID of the current WP Admin screen.
+	 *
+	 * @return array
+	 */
+	public static function receive_heartbeat( array $response, array $data, $screen_id ) {
+		if ( 'tools_page_smoothgenerator' !== $screen_id || empty( $data['smoothgenerator'] ) ) {
+			return $response;
+		}
+
+		$current_job = self::get_current_job();
+
+		if ( $current_job instanceof AsyncJob ) {
+			$response['smoothgenerator_async_job_progress'] = $current_job;
+			$response['smoothgenerator_ping_cron']          = site_url( 'wp-cron.php' );
+		} else {
+			$response['smoothgenerator_async_job_progress'] = 'complete';
+		}
+
+		return $response;
 	}
 
 	/**
@@ -78,50 +231,23 @@ class Settings {
 			check_admin_referer( 'generate', 'smoothgenerator_nonce' );
 			$num_to_generate = absint( $_POST['num_products_to_generate'] );
 			BatchProcessor::create_new_job( 'products', $num_to_generate );
-			add_action( 'admin_notices', array( __CLASS__, 'product_generating_notice' ) );
 		} else if ( ! empty( $_POST['generate_orders'] ) && ! empty( $_POST['num_orders_to_generate'] ) ) {
 			check_admin_referer( 'generate', 'smoothgenerator_nonce' );
 			$num_to_generate = absint( $_POST['num_orders_to_generate'] );
 			BatchProcessor::create_new_job( 'orders', $num_to_generate );
-			add_action( 'admin_notices', array( __CLASS__, 'order_generating_notice' ) );
-		} else if ( ! empty( $_POST['cancel_all_generations'] ) ) {
+		} else if ( ! empty( $_POST['cancel_job'] ) ) {
 			check_admin_referer( 'generate', 'smoothgenerator_nonce' );
 			BatchProcessor::delete_current_job();
-			add_action( 'admin_notices', array( __CLASS__, 'cancel_generations_notice' ) );
 		}
 	}
 
 	/**
-	 * Render notice about products generating.
+	 * Get the state of the current background job.
+	 *
+	 * @return AsyncJob|null
 	 */
-	public static function product_generating_notice() {
-		?>
-		<div class="notice notice-success is-dismissible">
-			<p>Generating products in the background . . . </p>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Render notice about orders generating.
-	 */
-	public static function order_generating_notice() {
-		?>
-		<div class="notice notice-success is-dismissible">
-			<p>Generating orders in the background . . . </p>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Render notice about canceling generations.
-	 */
-	public static function cancel_generations_notice() {
-		?>
-		<div class="notice notice-warning is-dismissible">
-			<p>Canceling all generations . . . </p>
-		</div>
-		<?php
+	protected static function get_current_job() {
+		return BatchProcessor::get_current_job();
 	}
 
 	/**
@@ -130,10 +256,11 @@ class Settings {
 	 * @return string
 	 */
 	protected static function while_you_wait() {
-		$content = '';
+		$current_job = self::get_current_job();
+		$content     = '';
 
-		if ( filter_input( INPUT_POST, 'smoothgenerator_nonce' ) ) {
-			if ( filter_input( INPUT_POST, 'cancel_all_generations' ) ) {
+		if ( filter_input( INPUT_POST, 'smoothgenerator_nonce' ) || $current_job instanceof AsyncJob ) {
+			if ( filter_input( INPUT_POST, 'cancel_job' ) ) {
 				$embed = 'NF9Y3GVuPfY';
 			} else {
 				$videos    = array(
@@ -148,11 +275,21 @@ class Settings {
 				}
 				$embed = $videos[ $next_wait ];
 				$next_wait ++;
-				setcookie( 'smoothgenerator_next_wait', $next_wait, time() + WEEK_IN_SECONDS, ADMIN_COOKIE_PATH, COOKIE_DOMAIN, is_ssl() );
+				setcookie(
+					'smoothgenerator_next_wait',
+					$next_wait,
+					array(
+						'expires'  => time() + WEEK_IN_SECONDS,
+						'path'     => ADMIN_COOKIE_PATH,
+						'domain'   => COOKIE_DOMAIN,
+						'secure'   => is_ssl(),
+						'samesite' => 'strict',
+					)
+				);
 			}
 
 			$content = <<<"EMBED"
-<div class="wp-block-embed__wrapper"><iframe width="560" height="315" src="https://www.youtube.com/embed/$embed?autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>></iframe></div>
+<div class="wp-block-embed__wrapper" style="margin: 2em 0;"><iframe width="560" height="315" src="https://www.youtube.com/embed/$embed?autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>></iframe></div>
 EMBED;
 		}
 
